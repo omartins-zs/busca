88cb2a0367ecc5f60c52851fc237db2830bbeb30
omartins-zs <gabrielmatheus209@gmail.com>
2021-12-11 09:53:31 -0300

    Criando Function Para Buscar

--- application/models/Busca_model.php
+++ application/models/Busca_model.php
@@ -1,7 +1,21 @@
 <?php
-Class Busca_model extends CI_Model {
+class Busca_model extends CI_Model
+{
 
-    public function usuarios(){
+    public function usuarios()
+    {
         return $this->db->get('usuarios')->result_array();
+    }
+
+    public function buscar($busca)
+    {
+        if (empty($busca))
+            return array();
+
+        $busca = $this->input->post('busca');
+
+        $this->db->like('nome', $busca);
+        $query = $this->db->get('usuarios');
+        return $query->result_array();
     }
 }
